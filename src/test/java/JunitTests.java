f47488a56b7773a4248d200b70ea97a956918950
pryceholmes <113060481+pryceholmes@users.noreply.github.com>
2024-06-18 10:58:33 -0500

    update build.gradle and rename main

---
+++ src/test/java/JunitTests.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JunitTests {
+
+    @Test
+    public void sampleTestMain() {
+        assertEquals(25, 10+15);
+    }
+}
